c59995cc59ae980e44c11a8f30deff837a2f07a4
Moniruzzaman Shanto <moniruzzaman@kolpolok.com>
2025-01-12 16:12:09 +0600

    update ServiceResource

--- app/Filament/Resources/ServiceResource.php
+++ app/Filament/Resources/ServiceResource.php
@@ -12,6 +12,14 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Filters\QueryBuilder;
+use Filament\Tables\Filters\QueryBuilder\Constraints\BooleanConstraint;
+use Filament\Tables\Filters\QueryBuilder\Constraints\DateConstraint;
+use Filament\Tables\Filters\QueryBuilder\Constraints\NumberConstraint;
+use Filament\Tables\Filters\QueryBuilder\Constraints\TextConstraint;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\Filter;
 
 class ServiceResource extends Resource
 {
@@ -23,7 +31,9 @@ class ServiceResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('price')->numeric()->required(),
+                Forms\Components\Textarea::make('description'),
             ]);
     }
 
@@ -31,11 +41,41 @@ class ServiceResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(isIndividual: true),
+                Tables\Columns\TextColumn::make('description')->sortable()->searchable(isIndividual: true),
+                Tables\Columns\TextColumn::make('price')->sortable()->searchable(isIndividual: true),
+                Tables\Columns\TextColumn::make('created_at')->dateTime()->toggleable(),
+                Tables\Columns\TextColumn::make('updated_at')->dateTime()->toggleable(),
             ])
             ->filters([
-                //
-            ])
+                Filter::make('created_at')
+                    ->form([
+                        Grid::make(2)
+                            ->schema([
+                                DatePicker::make('created_from')
+                                    ->label('Created From'),
+                                DatePicker::make('created_until')
+                                    ->label('Created Until')
+                            ])
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['created_until'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    }),
+                QueryBuilder::make()
+                    ->constraints([
+                        TextConstraint::make('name'),
+                        DateConstraint::make('created_at'),
+                    ])
+                    ->constraintPickerColumns(2),
+            ], layout: Tables\Enums\FiltersLayout::AboveContentCollapsible)
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
